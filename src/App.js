6d3029180f94298d99bc5de3efea3ebba931e194
tahagulbadan52 <gulbadantaha@gmail.com>
2022-06-16 11:38:30 +0400

    react app set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
 
+console.log(moment().format())
 function App() {
+  let today = moment().format("DD MMMM YYYY")
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is a test {today} <br/>
+      Day is {moment().day()}
+    
     </div>
   );
 }
